b822cf68ef7b8311085e588f7f4d7da5bf2f464d
Gravatinha <gleberphant@gmail.com>
2026-02-09 05:29:46 -0300

    chore: Configure Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-my-paint/', // Nome exato do repo no GitHub
   plugins: [react()],
 })
